4f79f8646597b7d25b0dd61620a18ff7674848d1
Tyler <ttangvz859@gmail.com>
2025-11-09 01:27:26 -0500

    wheel testing

--- driver_test.py
+++ driver_test.py
@@ -1,51 +1,72 @@
 from gpiozero import Motor, PWMOutputDevice
 from time import sleep
 
-# LEFT SIDE (two motors together on left driver)
-left_dir1 = 5
-left_dir2 = 6
-left_pwm_pin = 13
+# LEFT SIDE A
+left_dir1a = 6
+left_dir2a = 7
+left_pwm_pina = 18
 
-left_motor = Motor(forward=left_dir1, backward=left_dir2, pwm=False)
-left_pwm = PWMOutputDevice(left_pwm_pin, frequency=20000)  # quiet motor PWM
+left_motora = Motor(forward=left_dir1a, backward=left_dir2a, pwm=False)
+left_pwma = PWMOutputDevice(left_pwm_pina, frequency=2000)  # quiet motor PWM
 
-# RIGHT SIDE (two motors together on right driver)
-right_dir1 = 23
-right_dir2 = 24
-right_pwm_pin = 18
+# LEFT SIDE B
+left_dir1b = 16
+left_dir2b = 17
+left_pwm_pinb = 19
 
-right_motor = Motor(forward=right_dir1, backward=right_dir2, pwm=False)
-right_pwm = PWMOutputDevice(right_pwm_pin, frequency=20000)
+left_motorb = Motor(forward=left_dir1b, backward=left_dir2b, pwm=False)
+left_pwmb = PWMOutputDevice(left_pwm_pinb, frequency=2000)  # quiet motor PWM
+
+# # RIGHT SIDE (two motors together on right driver)
+# right_dir1a = 7
+# right_dir2a = 1
+# right_pwm_pina = 12
+# right_motora = Motor(forward=right_dir1a, backward=right_dir2a, pwm=False)
+# right_pwma = PWMOutputDevice(right_pwm_pina, frequency=2000)
+
+# # RIGHT SIDE (two motors together on right driver)
+# right_dir1b = 14
+# right_dir2b = 15
+# right_pwm_pinb = 18
+
+# right_motorb = Motor(forward=right_dir1b, backward=right_dir2b, pwm=False)
+# right_pwmb = PWMOutputDevice(right_pwm_pinb, frequency=2000)
 
 def forward(speed=0.8):
-    left_motor.forward()
-    right_motor.forward()
-    left_pwm.value = speed
-    right_pwm.value = speed
+    left_motora.forward()
+    # right_motora.forward()
+    # right_motora.forward()
+    left_pwma.value = speed
+    # right_pwma.value = speed
 
-def backward(speed=0.8):
-    left_motor.backward()
-    right_motor.backward()
-    left_pwm.value = speed
-    right_pwm.value = speed
+    left_motorb.forward()
+    # right_motorb.forward()
+    left_pwmb.value = speed
+    # right_pwmb.value = speed
 
-def turn_left(speed=0.8):
-    left_motor.backward()
-    right_motor.forward()
-    left_pwm.value = speed
-    right_pwm.value = speed
+# def backward(speed=0.8):
+#     left_motora.backward()
+#     right_motor.backward()
+#     left_pwma.value = speed
+#     right_pwm.value = speed
 
-def turn_right(speed=0.8):
-    left_motor.forward()
-    right_motor.backward()
-    left_pwm.value = speed
-    right_pwm.value = speed
+# def turn_left(speed=0.8):
+#     left_motora.backward()
+#     right_motor.forward()
+#     left_pwma.value = speed
+#     right_pwm.value = speed
 
-def stop():
-    left_motor.stop()
-    right_motor.stop()
-    left_pwm.value = 0
-    right_pwm.value = 0
+# def turn_right(speed=0.8):
+#     left_motora.forward()
+#     right_motor.backward()
+#     left_pwma.value = speed
+#     right_pwm.value = speed
+
+# def stop():
+#     left_motora.stop()
+#     right_motor.stop()
+#     left_pwma.value = 0
+#     right_pwm.value = 0
 
 # -------------------
 # TEST SEQUENCE
@@ -54,25 +75,25 @@ print("Forward...")
 forward(0.7)
 sleep(2)
 
-print("Stop...")
-stop()
-sleep(1)
+# print("Stop...")
+# stop()
+# sleep(1)
 
-print("Backward...")
-backward(0.7)
-sleep(2)
+# print("Backward...")
+# backward(0.7)
+# sleep(2)
 
-print("Stop...")
-stop()
-sleep(1)
+# print("Stop...")
+# stop()
+# sleep(1)
 
-print("Turn Left...")
-turn_left(0.7)
-sleep(1.5)
+# print("Turn Left...")
+# turn_left(0.7)
+# sleep(1.5)
 
-print("Turn Right...")
-turn_right(0.7)
-sleep(1.5)
+# print("Turn Right...")
+# turn_right(0.7)
+# sleep(1.5)
 
-print("Stop")
-stop()
+# print("Stop")
+# stop()
